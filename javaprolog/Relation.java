ab893e87e3e00f520d165e3ccf1a072d9b7def0e
Erik Ramqvist <erikramqvist@gmail.com>
2014-03-26 14:38:14 +0100

    Added Equals and hashcode to Entity and Relation

--- javaprolog/Relation.java
+++ javaprolog/Relation.java
@@ -33,6 +33,49 @@ public class Relation {
 	public Entity getEntityB() {
 		return b;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((a == null) ? 0 : a.hashCode());
+		result = prime * result + ((b == null) ? 0 : b.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Relation))
+			return false;
+		Relation other = (Relation) obj;
+		if (a == null) {
+			if (other.a != null)
+				return false;
+		} else if (!a.equals(other.a))
+			return false;
+		if (b == null) {
+			if (other.b != null)
+				return false;
+		} else if (!b.equals(other.b))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+	
+	
+	
 	
 	@Override
 	public String toString() {
