ac30c6bf2eb68fdc9005c9a77eb2034e35824937
Erik Ramqvist <erikramqvist@gmail.com>
2014-03-27 10:31:04 +0100

    Added Constraint checker class.

---
+++ javaprolog/ConstrainCheck.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+/**
+ * Class for checking constraints.
+ *
+ */
+public class ConstrainCheck {
+	
+	public static boolean isValidColumn(List<Entity> entityList) {
+		return true;
+	}
+	
+	public static boolean isValidRelations(List<Relation> entityList) {
+		return true;
+	}
+}
