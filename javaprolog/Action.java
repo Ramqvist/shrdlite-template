4eb352a0e75eb363a4572c954c3ef6ffb807d314
antonwestberg <antonwestberg_@hotmail.com>
2014-03-30 12:51:01 +0200

    Starting out with the planner and trying command line git (cross you fingers!)

---
+++ javaprolog/Action.java
@@ -0,0 +1,25 @@
+
+public class Action {
+	public enum COMMAND{
+		PICK,
+		DROP
+	}
+	public final COMMAND command;
+	public final int column;
+	
+	public Action(COMMAND com, int col){
+		command = com;
+		column = col;
+	}
+	
+	public String toString(){
+		if(command == COMMAND.PICK){
+			return "pick " + column;
+		}else{
+			return "drop " + column;
+		}
+		
+		
+		
+	}
+}
--- javaprolog/Plan.java
+++ javaprolog/Plan.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Plan {
 
+	public final List<Action> actions;
+	
+	public Plan(){
+		actions = new ArrayList<Action>();
+	}
+	
+	public void add(Action a){
+		actions.add(a);
+	}
+	
 	public boolean isEmpty() {
 		return true; // TODO
 	}
