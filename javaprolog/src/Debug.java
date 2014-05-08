5afde787853ea6436fbb10870c6f4617ec51ddc5
Ramqvist <erikramqvist@gmail.com>
2014-05-08 11:33:34 +0200

    Added benchmark functions to shrdlite.java

--- javaprolog/src/Debug.java
+++ javaprolog/src/Debug.java
@@ -6,7 +6,8 @@ package src;
  */
 public class Debug {
 
-	public static boolean debug = true;
+	public static final boolean debug = true;
+	public static final boolean benchmark = false;
 
 	/**
 	 * Prints the given message to standard output if the Debug.debug variable
